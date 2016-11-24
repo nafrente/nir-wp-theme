9217af9a4f61ecad3e88bac57dfdb6ac6bc59fe6
Herton <herton@mentel.com>
2016-11-24 15:14:41 -0500

    Replacing missing less with scss

--- wp-content/themes/nir-theme/index.php
+++ wp-content/themes/nir-theme/index.php
@@ -11,7 +11,7 @@
 <header name="top" style="background-color: #26C1DC; background-image:url('<?php echo get_template_directory_uri(); ?>/img/homepage1.jpg');">
     <div class="container">
     <nav>
-        <div class="branding"><a href="index.php"><?php echo file_get_contents("svg/nir-01.svg"); ?><span>Newport Integrated <br />Resiliency</span></a></div>
+        <div class="branding"><a href="index.php"><?php echo file_get_contents( get_template_directory_uri() . "/svg/nir-01.svg"); ?><span>Newport Integrated <br />Resiliency</span></a></div>
         <ul>
             <li><a href="#">About</a></li>
             <li><a href="#">Model</a></li>
