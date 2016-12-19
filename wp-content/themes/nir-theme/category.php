7d5f3b5960a09013b915bd0c277b8bc015c43eda
Herton <herton@mentel.com>
2016-12-19 16:11:43 -0500

    Fixing category page filtering post types

--- wp-content/themes/nir-theme/category.php
+++ wp-content/themes/nir-theme/category.php
@@ -14,27 +14,18 @@
 
             <div class="posts-list">
                 <?php
-
-                $args2 = array( 'posts_per_page' => -1);
+                //Getting category
+                $cat_ID = get_query_var('cat');
+                $post_types = array('post', 'project');
+                $args2 = array( 'posts_per_page' => -1, 'cat' => $cat_ID, 'post_type' => $post_types );
                 $loop2 = new WP_Query( $args2 );
-                $how_many_projects = 10; //The number of posts to be shown
                 while ( $loop2->have_posts() ) : $loop2->the_post();
-                    $post_id = get_the_ID();
-                    /*if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                        $large_image_url = get_the_post_thumbnail_url();
-                    }
-                    else{
-                        $large_image_url =  the_post_thumbnail_url() . '/default.jpg';
-                    }
-                    */
 
                     echo '<div class="single-list">';
                     echo '<h2>'.  get_the_title() . '</h2>';
                     echo '<p>'.  get_the_excerpt() .  '</p>';
                     echo '<a href="' . get_permalink() . '" class="btn">'. __('Read More', 'nir-theme') .'</a>';
                     echo '</div>';
-                    $how_many_projects = $how_many_projects - 1;
-
 
                 endwhile;
 
