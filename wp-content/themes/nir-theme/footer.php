e376575757dff08b006f85bdba6eec1d5dd46cb3
Herton <herton@mentel.com>
2016-12-19 16:22:58 -0500

    Replacing footer arrow up

--- wp-content/themes/nir-theme/footer.php
+++ wp-content/themes/nir-theme/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
-        <a id="top-link" href="#">↑ Return to Top</a>
+        <a id="top-link" href="#"><i class="fa fa-chevron-up" aria-hidden="true"></i> Return to Top</a>
         <p class="copyright">&copy; 2015 Worldways, Inc.</p>
     </div>
 </footer>
